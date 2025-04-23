bde21c960f1ac2238b9fea3182b93bb369aa5ace
Ikitursa <kirilenko_yin@outlook.com>
2025-04-23 11:36:43 +0800

    Update SSD1311.h

--- src/SSD1311.h
+++ src/SSD1311.h
@@ -1,27 +1,32 @@
 #include "Arduino.h"
 #include "Wire.h"
 
-#define SSD1311_ADDR 0x3C
-//#define SSD1311_ADDR 0x3D
+#define DISPLAY_CLEAR 		0x01
+#define DISPLAY_ON 			0x0C
+#define DISPLAY_OFF 		0x08
+#define DISPLAY_CONTRAST 	0x81
 
-#ifndef RES
-#define RES 7
-#endif
+// 0x3C / 0x3D
 
 class SSD1311
 {
-public:
-	SSD1311();
-	void SWRES();
-	void init();
-	void clear();
-	void setContrast(unsigned char contrast);
-	void setCursor(uint8_t, uint8_t);
-	void sendCMD(unsigned char command);
-	void sendData(unsigned char data);
-	void sendFloat(uint8_t col, uint8_t row, float digit, uint8_t dec, uint8_t nad);
-	void sendStr(uint8_t col, uint8_t row, const char *String);
-	void scrollStr(byte row, char *message, unsigned int time);
-	void dispOFF();
-	void dispON();
+	public:
+		SSD1311(uint8_t addr, uint8_t res, uint8_t cols = 16, uint8_t rows = 2);
+		void init();
+		void clear();
+		void setContrast(unsigned char contrast);
+		void setCursor(uint8_t, uint8_t);
+		void sendCMD(unsigned char command);
+		void sendData(unsigned char data);
+		void sendFloat(uint8_t col, uint8_t row, float digit, uint8_t dec, uint8_t nad);
+		void sendStr(uint8_t col, uint8_t row, const char *String);
+		void scrollStr(byte row, const char *message, unsigned int speed, uint8_t displayWidth = 16);
+		void dispOFF();
+		void dispON();
+
+	private:
+		uint8_t address;
+		uint8_t resetPin;
+		uint8_t numCols;
+		uint8_t numRows;
 };
